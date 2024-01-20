6befcfe4c469f4f5b2eee4ebd81cd9954362d41d
Knowble <knowble.off@gmail.com>
2024-01-20 23:26:48 +0700

    up

--- script.js
+++ script.js
@@ -1,23 +1,4 @@
 // time counter
-function pad(val) {
-  var valString = val + "";
-  if (valString.length < 2) {
-    return "0" + valString;
-  } else {
-    return valString;
-  }
-}
-
-var totalSeconds = 0;
-
-setInterval(setTime, 1000);
-
-function setTime() {
-  ++totalSeconds;
-  document.getElementById("seconds").innerHTML = pad(totalSeconds % 60);
-  document.getElementById("minutes").innerHTML = pad(parseInt(totalSeconds / 60));
-}
-
 function checkTime(i) {
   if (i < 10) {
     i = "0" + i;
